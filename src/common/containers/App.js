6b3bbf5527f176c9b23e4c6a7d90413511b9db01
Shaun Sweet <shaun@shaunsweet.com>
2018-02-13 00:21:17 -0700

    fix: change app title

--- src/common/containers/App.js
+++ src/common/containers/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       <Provider store={store}>
         <div className='App'>
           <header className='App-header'>
-            <h1 className='App-title'>Welcome to React</h1>
+            <h1 className='App-title'>SALAD RABBIT YAY</h1>
           </header>
           <Router history={history}>
             <div className='content'>
