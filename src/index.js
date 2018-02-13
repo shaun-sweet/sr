1bace895666ac066a596ca81f68a2cdd107dee4e
Shaun Sweet <shaun@shaunsweet.com>
2018-02-12 21:48:54 -0700

    chore: switch to CRA

--- src/index.js
+++ src/index.js
@@ -1,32 +1,8 @@
-// Summary:
-//   This is the entry of the application, works together with index.html.
-
-import 'babel-polyfill';
 import React from 'react';
-import { AppContainer } from 'react-hot-loader';
-import { render } from 'react-dom';
-import configStore from './common/configStore';
-import routeConfig from './common/routeConfig';
-import Root from './Root';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-const store = configStore();
-
-function renderApp(app) {
-  render(
-    <AppContainer>
-      {app}
-    </AppContainer>,
-    document.getElementById('react-root')
-  );
-}
-
-renderApp(<Root store={store} routeConfig={routeConfig} />);
-
-// Hot Module Replacement API
-/* istanbul ignore if  */
-if (module.hot) {
-  module.hot.accept('./common/routeConfig', () => {
-    const nextRouteConfig = require('./common/routeConfig').default; // eslint-disable-line
-    renderApp(<Root store={store} routeConfig={nextRouteConfig} />);
-  });
-}
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
